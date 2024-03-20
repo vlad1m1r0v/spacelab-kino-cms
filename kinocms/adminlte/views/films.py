64aec3add4c140d09e11b11bf7f1408bb774dbab
ant100pshnik <mypochtav@gmail.com>
2024-03-20 14:17:04 +0200

    fix bug with incorrect film images loading

--- kinocms/adminlte/views/films.py
+++ kinocms/adminlte/views/films.py
@@ -1,3 +1,5 @@
+from django.contrib import messages
+from django.shortcuts import redirect
 from django.views.generic import TemplateView, CreateView
 
 from adminlte.decorators import admin_only
@@ -24,3 +26,24 @@ class CreateFilmView(CreateView):
         return self.render_to_response(
             self.get_context_data(form=form, formset=formset)
         )
+
+    def post(self, request, *args, **kwargs):
+        self.object = None
+        form = FilmForm(request.POST, request.FILES)
+        formset = FilmImageFormSet(request.POST, request.FILES)
+
+        if form.is_valid() and formset.is_valid():
+            film = form.save()
+            film.save()
+
+            for form in formset:
+                if form.is_valid():
+                    film_image = form.save(commit=False)
+                    film_image.film = film
+                    film_image.save()
+
+            messages.success(request, "Film was created successfully")
+            return redirect('films:index')
+
+        messages.error(request, "Some errors occurred while creating film")
+        return redirect('films:index')
